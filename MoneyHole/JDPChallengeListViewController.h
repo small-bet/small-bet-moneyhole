1696ab9d083f02ac716f7e593238fac1db1bc068
Joel Parsons <joel@brightfire.eu>
2013-08-30 16:40:31 +0100

    added explanation that these classes are now provided by small-bet, if the developer wants to use them.

--- MoneyHole/JDPChallengeListViewController.h
+++ MoneyHole/JDPChallengeListViewController.h
@@ -9,6 +9,20 @@
 #import <UIKit/UIKit.h>
 #import "JDPChallengeSelectionDelegate.h"
 
+/*
+
+
+This class is an example of how to implement the list of challenges using the small bet SDK. 
+
+
+You dont need to implement these screens in your app. You can use the SBChallengeListViewController (which was actually mostly based off this code)
+
+This remains in the Moneyhole code as a demo to some of the interface customization you can achieve with the small-bet SDK.
+
+ 
+ 
+ 
+ */
 @interface JDPChallengeListViewController : UITableViewController
 <JDPChallengeSelectionDelegate>
 
